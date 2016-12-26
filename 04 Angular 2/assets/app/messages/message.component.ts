4a9cbc42d6f988de0c51784d6631cf9a8e70fe9a
johnnyzh82 <johnnyzh82@gmail.com>
2016-12-26 15:06:06 -0500

    added angular2 component

--- 04 Angular 2/assets/app/messages/message.component.ts
+++ 04 Angular 2/assets/app/messages/message.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Message } from "./message.model";
 
 @Component({
     selector: 'app-message',
-    templateUrl: './message.component.html'
+    templateUrl: './message.component.html',
+        styles: [`
+        .author{
+            display: inline-block;
+            font-style: italic;
+            font-size: 12px;
+            width: 80%;
+        }
+        .config{
+            display: inline-block;
+            text-align: right;
+            font-size: 12px;
+            width: 19%;
+        }
+    `]
 })
 
 export class MessageComponent {
-
+    //@input() allows pass argument from outside
+    @Input() message: Message;
+    @Output() editClicked = new EventEmitter<string>();
+    
+    onEdit() {
+        this.editClicked.emit('a new value');
+    }
 }
