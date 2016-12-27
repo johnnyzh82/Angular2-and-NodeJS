3810380c8250d8f36d5d8d8ac5502775109a4962
johnnyzh82 <johnnyzh82@gmail.com>
2016-12-26 19:33:28 -0500

    Section 5 start

---
+++ 05 Angular 2 + NodeJS/assets/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule }                 from '@angular/core';
+import { BrowserModule }            from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent }             from "./app.component";
+import { MessageComponent }         from "./messages/message.component";
+import { MessageListComponent }     from "./messages/message-list.component";
+import { MessageInputComponent }    from "./messages/message-input.component";
+import { MessagesComponent }        from "./messages/messages.components";
+
+import { AuthenticationComponent }  from "./auth/authentication.component";
+import { SigninComponent }          from "./auth/signin.component";
+import { SignupComponent }          from "./auth/signup.component";
+import { LogoutComponent }          from "./auth/logout.component";
+
+import { HeaderComponent }          from "./header.component";
+import { routing }                  from "./app.routing";
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        MessageComponent,
+        MessageListComponent,
+        MessageInputComponent,
+        MessagesComponent,
+        AuthenticationComponent,
+        SigninComponent,
+        SignupComponent,
+        LogoutComponent,
+        HeaderComponent,
+    ],
+    imports: [BrowserModule, FormsModule, routing, ReactiveFormsModule ],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+
+}
