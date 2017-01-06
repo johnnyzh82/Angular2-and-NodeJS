86fc770b56e6d265a7a76ad56806d837226d0e06
johnnyzh82 <johnnyzh82@gmail.com>
2017-01-05 23:44:15 -0500

    backend sign in

--- 06 Users and Authentication/routes/user.js
+++ 06 Users and Authentication/routes/user.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
 var User = require('../models/user');
 
 //password encryption/decryption using 3rd party lib
+//one way encryption (can't decrypt)
 //npm install --save bcryptjs
 router.post('/', function (req, res, next) {
     var user = new User({
@@ -28,4 +30,35 @@ router.post('/', function (req, res, next) {
     });
 });
 
+router.post('/signin', function(req, res, next){
+    User.findOne({ email: req.body.email }, function(err, user){
+        if(err){
+            return res.status(500).json({
+                title: 'An error occurred',
+                error: err
+            });
+        }
+        if(!user){
+            return res.status(401).json({
+                title: 'Login Failed',
+                error: { message: 'Invalid login credentials' }
+            });
+        }
+        if(!bcrypt.compareSync(req.body.password, user.password)){
+            return res.status(401).json({
+                title: 'Login Failed',
+                error: { message: 'Invalid login credentials' }
+            });
+        }
+        //create a token JWT
+        //client can use token for request
+        //create a new token
+        var token = jwt.sign({user: user}, 'secret', {expiresIn: 7200});
+        res.status(200).json({
+            message: 'Successfully logged in',
+            token: token,
+            userId: user._id
+        });
+    });
+});
 module.exports = router;
